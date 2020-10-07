8b936fd35df919cb0502da679180cca4bb4626f5
ACEisANARCHY <72099703+ACEisANARCHY@users.noreply.github.com>
2020-10-07 18:56:30 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCoZK8C3u3shgNQjXc7j8FQDt2D91bJHrDUrwyRG4DOp7dDVm+WUb15lR5RA6ZaU0fzSve2iU40Vs0J28rV7qUEsm7Z4AyUcBDltG/WR4hP5218IsAx4u40SyMzGWRuRc7SZj4+jJXDIbekrn98ekMUkmILavT/rawa9lScacSIF1P1QF0c/BLZrEkmTS//dfohWiMltGo3QZA+MwHJcPzD1Uh/P5Te6iC8YYUAPXz/OHop4mvydQQm90Y2ClTttqYJZ/ogbWLRy33TUlNvvfvJcgU7/dFbJNYuD0P77tj7BEhqFWpSNAiAp9sTkupNYDs0crnUWRKgZ6EpnFtaNto9',
 	}  
 }
